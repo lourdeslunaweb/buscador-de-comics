ba8b80311bd8e89579bea6cdd0daaf378e9ad357
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2021-09-10 23:22:41 -0300

    working on filter by type, add footer and pass page buttons

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,34 +1,9 @@
-// Your public key
-// 06e295e3c238e43e31ef140c424be15b
-// Your private key
-// fb7c2312c6804213e326c91c5d7d6683169968ae
-// 1fb7c2312c6804213e326c91c5d7d6683169968ae06e295e3c238e43e31ef140c424be15b
-// hash : 953044dd6187bef3005abdd0e7cf0d93
-//definir types
-var getDataCharacter = {
-    render: function () {
-        var urlAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
-        var marvelCards = document.getElementById("marvel-cards");
-        var contentHTML = "";
-        fetch(urlAPI)
-            .then(function (res) { return res.json(); })
-            .then(function (json) {
-            for (var _i = 0, _a = json.data.results; _i < _a.length; _i++) {
-                var hero = _a[_i];
-                var urlHero = hero.urls[0].url;
-                var thumb = hero.thumbnail;
-                contentHTML += "\n                <div class=\"card-div\">\n                    <a href=\"" + urlHero + "\" target=\"_blank\">\n                        <img src=\"" + thumb.path + "." + thumb.extension + "\" alt=\"" + hero.name + "\"  class=\"card-home\">\n                    </a>\n                    <h3>" + hero.name + "</h3>\n                </div>";
-            }
-            marvelCards.innerHTML = contentHTML;
-            // console.log(json,'RES.JSON')
-        });
-    }
-};
-getDataCharacter.render();
+var typeFilter = document.getElementById("type-filter");
+var marvelCards = document.getElementById("marvel-cards");
+var searchBtn = document.getElementById("search-button");
 var getDataComics = {
     render: function () {
         var urlAPI = "https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
-        var marvelCards = document.getElementById("marvel-cards2");
         var contentHTML = "";
         fetch(urlAPI)
             .then(function (res) { return res.json(); })
@@ -40,8 +15,45 @@ var getDataComics = {
                 contentHTML += "\n                <div class=\"card-div\">\n                    <a href=\"" + urlComic + "\" target=\"_blank\">\n                        <img src=\"" + thumb.path + "." + thumb.extension + "\" alt=\"" + comic.title + "\"  class=\"card-home\">\n                    </a>\n                    <h3>" + comic.title + "</h3>\n                </div>";
             }
             marvelCards.innerHTML = contentHTML;
-            console.log(json, 'RES.JSON');
+            console.log("COMICS", json);
         });
     }
 };
-getDataComics.render();
+var getDataCharacter = {
+    render: function () {
+        var urlAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
+        var contentHTML = "";
+        fetch(urlAPI)
+            .then(function (res) { return res.json(); })
+            .then(function (json) {
+            for (var _i = 0, _a = json.data.results; _i < _a.length; _i++) {
+                var hero = _a[_i];
+                var urlHero = hero.urls[0].url;
+                var thumb = hero.thumbnail;
+                contentHTML += "\n                <div class=\"card-div\">\n                    <a href=\"" + urlHero + "\" target=\"_blank\">\n                        <img src=\"" + thumb.path + "." + thumb.extension + "\" alt=\"" + hero.name + "\"  class=\"card-home\">\n                    </a>\n                    <h3>" + hero.name + "</h3>\n                </div>";
+            }
+            marvelCards.innerHTML = contentHTML;
+            console.log("PERSONAJES", json);
+        });
+    }
+};
+var filterByType = function (e) {
+    if (typeFilter.value === "comics") {
+        getDataComics.render();
+    }
+    else if (typeFilter.value === "personajes") {
+        getDataCharacter.render();
+    }
+};
+searchBtn.addEventListener("click", filterByType);
+// Your public key
+// 06e295e3c238e43e31ef140c424be15b
+// Your private key
+// fb7c2312c6804213e326c91c5d7d6683169968ae
+// 1fb7c2312c6804213e326c91c5d7d6683169968ae06e295e3c238e43e31ef140c424be15b
+// hash : 953044dd6187bef3005abdd0e7cf0d93
+//definir types
+var indexInit = function () {
+    getDataComics.render();
+};
+indexInit();
--- assets/script/script.ts
+++ assets/script/script.ts
@@ -1,43 +1,10 @@
-// Your public key
-// 06e295e3c238e43e31ef140c424be15b
-
-// Your private key
-// fb7c2312c6804213e326c91c5d7d6683169968ae
-
-// 1fb7c2312c6804213e326c91c5d7d6683169968ae06e295e3c238e43e31ef140c424be15b
-// hash : 953044dd6187bef3005abdd0e7cf0d93
-
-//definir types
-const getDataCharacter = {
-    render: () => {
-        const urlAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
-        const marvelCards = document.getElementById("marvel-cards");
-        let contentHTML = "";
-        fetch(urlAPI)
-        .then(res => res.json())
-        .then((json) => {
-            for(const hero of json.data.results){
-                let urlHero = hero.urls[0].url;
-                let thumb = hero.thumbnail;
-                contentHTML+= `
-                <div class="card-div">
-                    <a href="${urlHero}" target="_blank">
-                        <img src="${thumb.path}.${thumb.extension}" alt="${hero.name}"  class="card-home">
-                    </a>
-                    <h3>${hero.name}</h3>
-                </div>`;
-            }
-            marvelCards.innerHTML = contentHTML;
-            // console.log(json,'RES.JSON')
-        })
-    }
-}
-getDataCharacter.render();
+const typeFilter = document.getElementById("type-filter");
+const marvelCards = document.getElementById("marvel-cards");
+const searchBtn = document.getElementById("search-button");
 
 const getDataComics = {
     render: () => {
         const urlAPI = "https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
-        const marvelCards = document.getElementById("marvel-cards2");
         let contentHTML = "";
         fetch(urlAPI)
         .then(res => res.json())
@@ -54,9 +21,65 @@ const getDataComics = {
                 </div>`;
             }
             marvelCards.innerHTML = contentHTML;
-            console.log(json,'RES.JSON')
+            console.log("COMICS", json)
         })
     }
 }
 
-getDataComics.render();
+const getDataCharacter = {
+    render: () => {
+        const urlAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
+        let contentHTML = "";
+        fetch(urlAPI)
+        .then(res => res.json())
+        .then((json) => {
+            for(const hero of json.data.results){
+                let urlHero = hero.urls[0].url;
+                let thumb = hero.thumbnail;
+                contentHTML+= `
+                <div class="card-div">
+                    <a href="${urlHero}" target="_blank">
+                        <img src="${thumb.path}.${thumb.extension}" alt="${hero.name}"  class="card-home">
+                    </a>
+                    <h3>${hero.name}</h3>
+                </div>`;
+            }
+            marvelCards.innerHTML = contentHTML;
+            console.log("PERSONAJES", json)
+        })
+    }
+}
+
+
+
+
+const filterByType = (e) =>{
+    if(typeFilter.value === "comics"){
+        getDataComics.render();
+    } else if (typeFilter.value === "personajes"){
+        getDataCharacter.render();
+    } 
+}
+
+searchBtn.addEventListener("click", filterByType);
+
+
+
+
+
+// Your public key
+// 06e295e3c238e43e31ef140c424be15b
+
+// Your private key
+// fb7c2312c6804213e326c91c5d7d6683169968ae
+
+// 1fb7c2312c6804213e326c91c5d7d6683169968ae06e295e3c238e43e31ef140c424be15b
+// hash : 953044dd6187bef3005abdd0e7cf0d93
+
+//definir types
+
+
+const indexInit =() =>{
+    getDataComics.render();
+}
+indexInit();
