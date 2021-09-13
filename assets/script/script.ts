ec194afeb5e870cb593e0c9ce8047ffc0fa0c1be
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2021-09-13 19:56:21 -0300

    work on next and prev btn, search box filter

--- assets/script/script.ts
+++ assets/script/script.ts
@@ -1,85 +1,135 @@
+// Your public key
+// 06e295e3c238e43e31ef140c424be15b
+// Your private key
+// fb7c2312c6804213e326c91c5d7d6683169968ae
+// 1fb7c2312c6804213e326c91c5d7d6683169968ae06e295e3c238e43e31ef140c424be15b
+// hash : 953044dd6187bef3005abdd0e7cf0d93
+const baseUrl: string = "https://gateway.marvel.com:443/v1/public/";
+const apiKey: string = "06e295e3c238e43e31ef140c424be15b";
+const hash: string = "1eee8ff490d4a973b65d6f613e9569ff";
+const searchParam = new URLSearchParams(window.location.search);
+let pageNumber: number = 1;
+const limit = 20;
+let offset = 0;
+let pagination;
+// var noticias = ["Noticia 1", "Noticia 2", "Noticia 3"]; esto seria nuestro json.data.results
+// var pageCont = Math.ceil(noticias.length / pageSize); se define adentro del fetch
 const typeFilter = document.getElementById("type-filter");
 const marvelCards = document.getElementById("marvel-cards");
+const results = document.getElementById("results");
 const searchBtn = document.getElementById("search-button");
+const nextBtn = document.getElementById("next-btn");
+const prevBtn = document.getElementById("prev-btn");
+const firstPageBtn = document.getElementById("first-page-btn");
+const older = document.getElementById("older");
+const newer = document.getElementById("newer");
+const sortFilter = document.getElementById("sort-filter");
 
+
+// *** GET COMICS DATA ***
 const getDataComics = {
-    render: () => {
-        const urlAPI = "https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
+    render: (offset) => {
+        const urlAPI = `${baseUrl}comics?ts=1&apikey=${apiKey}&hash=${hash}&limit=${limit}&offset=${offset}`;
         let contentHTML = "";
         fetch(urlAPI)
-        .then(res => res.json())
-        .then((json) => {
-            for(const comic of json.data.results){
-                let urlComic = comic.urls[0].url;
-                let thumb = comic.thumbnail;
-                contentHTML+= `
+            .then(res => res.json())
+            .then((json) => {
+                console.log("COMICS", json)
+                // Display results
+                for (const comic of json.data.results) {
+                    let urlComic = comic.urls[0].url;
+                    let thumb = comic.thumbnail;
+                    contentHTML += `
                 <div class="card-div">
                     <a href="${urlComic}" target="_blank">
                         <img src="${thumb.path}.${thumb.extension}" alt="${comic.title}"  class="card-home">
                     </a>
                     <h3>${comic.title}</h3>
                 </div>`;
-            }
-            marvelCards.innerHTML = contentHTML;
-            console.log("COMICS", json)
-        })
+                }
+                marvelCards.innerHTML = contentHTML;
+                // Pagination
+                const resNumber = json.data.total;
+                results.innerText = `${resNumber} resultados`;
+                const totalPages = Math.ceil(resNumber / limit);
+            })
     }
 }
 
+// *** GET CHARACTERS DATA ***
 const getDataCharacter = {
     render: () => {
-        const urlAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=06e295e3c238e43e31ef140c424be15b&hash=1eee8ff490d4a973b65d6f613e9569ff";
+        const urlAPI = `${baseUrl}characters?ts=1&apikey=${apiKey}&hash=${hash}&limit=${limit}&offset=${offset}`;
         let contentHTML = "";
         fetch(urlAPI)
-        .then(res => res.json())
-        .then((json) => {
-            for(const hero of json.data.results){
-                let urlHero = hero.urls[0].url;
-                let thumb = hero.thumbnail;
-                contentHTML+= `
+            .then(res => res.json())
+            .then((json) => {
+                const resNumber = json.data.total
+                results.innerText = `${resNumber} resultados`
+                for (const hero of json.data.results) {
+                    let urlHero = hero.urls[0].url;
+                    let thumb = hero.thumbnail;
+                    contentHTML += `
                 <div class="card-div">
                     <a href="${urlHero}" target="_blank">
                         <img src="${thumb.path}.${thumb.extension}" alt="${hero.name}"  class="card-home">
                     </a>
                     <h3>${hero.name}</h3>
                 </div>`;
-            }
-            marvelCards.innerHTML = contentHTML;
-            console.log("PERSONAJES", json)
-        })
+                }
+                marvelCards.innerHTML = contentHTML;
+                console.log("PERSONAJES", json)
+            })
     }
 }
 
 
-
-
-const filterByType = (e) =>{
-    if(typeFilter.value === "comics"){
-        getDataComics.render();
-    } else if (typeFilter.value === "personajes"){
-        getDataCharacter.render();
-    } 
+// *** PAGINATION CONTROLS ***
+const nextPage = () => {
+    offset += 20;
+    getDataComics.render(offset);
+    if(offset >= 20){
+        prevBtn.classList.remove("hidden");
+        firstPageBtn.classList.remove("hidden");
+    }
 }
+nextBtn.addEventListener("click", nextPage);
 
-searchBtn.addEventListener("click", filterByType);
-
-
-
-
-
-// Your public key
-// 06e295e3c238e43e31ef140c424be15b
-
-// Your private key
-// fb7c2312c6804213e326c91c5d7d6683169968ae
-
-// 1fb7c2312c6804213e326c91c5d7d6683169968ae06e295e3c238e43e31ef140c424be15b
-// hash : 953044dd6187bef3005abdd0e7cf0d93
-
-//definir types
+const prevPage = () => {
+    offset -= 20;
+    if (offset === 0) {
+        prevBtn.classList.add("hidden");
+        firstPageBtn.classList.add("hidden");
+    } else {
+        getDataComics.render(offset);
+    }
+}
+prevBtn.addEventListener("click", prevPage);
 
 
-const indexInit =() =>{
-    getDataComics.render();
+// ******************
+// *** SEARCH BOX ***
+// ******************
+
+// *** FILTERS ***
+const filterByType = (e) => {
+    if (typeFilter.value === "comics") {
+        older.classList.remove("hidden");
+        newer.classList.remove("hidden");
+        getDataComics.render();
+    } else if (typeFilter.value === "personajes") {
+        older.classList.add("hidden");
+        newer.classList.add("hidden");
+        getDataCharacter.render();
+    }
+}
+typeFilter.addEventListener("change", filterByType);
+
+
+// *** INIT FUNCTION ***
+const indexInit = () => {
+    getDataComics.render(offset);
+    prevBtn.classList.add("hidden");
+    firstPageBtn.classList.add("hidden");
 }
 indexInit();
