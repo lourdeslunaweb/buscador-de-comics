fae84c7b179ef2c529efa210d278d9d0a241cb6a
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2021-09-20 14:46:18 -0300

    work on data.html: testing fetch with characters url api

--- assets/script/data.js
+++ assets/script/data.js
@@ -8,19 +8,42 @@ var title = params.get("title");
 var date = new Date(params.get("published")).toLocaleDateString();
 var creator = params.get("creator");
 var description = params.get("description");
-var characters = params.get("characters");
-console.log("Creator:", creator);
-console.log("----------------------------");
-console.log("Characters", characters);
+var charactersUrl = params.get("characters");
+var typeData = params.get("type");
 //Nodes
 var imgData = document.getElementById("img-data");
 var titleData = document.getElementById("title");
 var dateData = document.getElementById("date-data");
 var creatorData = document.getElementById("creator-data");
 var descriptionData = document.getElementById("description");
+var subTitleBelow = document.getElementById("subtitle-below");
+var resultsBelow = document.getElementById("results-below");
+var cardsBelow = document.getElementById("cards-below");
+var contentCardsBelow = "";
 //Inner HTML and set attribute
 imgData.setAttribute('src', "" + imgSrc);
 titleData.innerHTML = "" + title;
 dateData.innerHTML = "" + date;
 creatorData.innerHTML = "" + creator;
 descriptionData.innerHTML = "" + description;
+if (typeData === "comics") {
+    subTitleBelow.innerHTML = "Personajes";
+}
+else if (typeData === "characters") {
+    subTitleBelow.innerHTML = "Comics";
+}
+// Fetch
+var urlData = charactersUrl + "?ts=1&apikey=" + apiKey + "&hash=" + hash;
+fetch(urlData)
+    .then(function (res) { return res.json(); })
+    .then(function (characters) {
+    console.log(characters.data);
+    var charactersArray = characters.data.results;
+    console.log(charactersArray.length);
+    if (charactersArray.length === 0) {
+        resultsBelow.innerText = "0 resultados";
+        cardsBelow.innerHTML = "<h2> No se han encontrado resultados </h2>";
+    }
+    else {
+    }
+});
